e01b4b87d10adea70271af7df13fc3b97349bae7
bmellado222 <bmellado222@gmail.com>
2025-01-21 23:39:33 -0500

    Add files via upload
    This is the Spring Boot application of my Janken Website Project.

---
+++ JankenWebsite/src/main/java/com/mellado/janken/jankenwebsite/JankenWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.mellado.janken.jankenwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankenWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JankenWebsiteApplication.class, args);
+    }
+
+}
