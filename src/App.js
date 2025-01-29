8914c7b43e0509b83ca818a7375b8d13d400a8c2
bmellado222 <bmellado222@gmail.com>
2025-01-28 22:36:19 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingHeader from './components/LandingHeader'
 import LandingContent from './components/LandingContent'
 import './App.css';
+import './index.css'
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
 
 function App() {
   return (
--- src/components/LandingHeader.js
+++ src/components/LandingHeader.js
@@ -1,17 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { FaBars, FaFistRaised  } from 'react-icons/fa';
 import '../styles/LandingHeader.css';
+import { HiMagnifyingGlass } from "react-icons/hi2";
+import { BsBellFill } from "react-icons/bs";
 
-function LandingHeader() {
+
+const LandingHeader = () => {
+    const [isMobile, setIsMobile] = useState(false);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const handleResize = () => {
+        setIsMobile(window.innerWidth <= 1100);
+    };
+
+    useEffect(() => {
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [])
+
+    const backHome = () => {
+        if (location.pathname === '/') {
+            if(window.scrollY !== 0) {
+                window.scrollTo({ top:0, behavior: 'smooth'})
+            }
+        } else {
+            navigate('/');
+        }
+    }
+
     return (
         <header className="landing-header">
-            <div className="container">
-                <h1>Janken</h1>
-                <nav className="nav-bar">
-
-                </nav>
+            <div className="header-content">
+                {isMobile ? (
+                    <>
+                        <div className="left-side">
+                        <button className="hamburger">
+                            <FaBars/>
+                        </button>
+                        <button className="website-title" onClick={backHome}>
+                            Janken
+                        </button>
+                        </div>
+                        <div className="right-side">
+                        <button className="magnifying-glass">
+                            <HiMagnifyingGlass/>
+                        </button>
+                        <button className="fist">
+                            <FaFistRaised />
+                        </button>
+                        <button className="bell">
+                            <BsBellFill />
+                        </button>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                    <div className="left-side">
+                    <button className="website-title" onClick={backHome}>
+                            Janken
+                        </button>
+                        <nav className="nav">
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="#characters">Characters</a></li>
+                                <li><a href="#glossary">Glossary</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="right-side">
+                    <input type="text" className="search-bar" placeholder="Search..."/>
+                        <button className="fist">
+                            <FaFistRaised />
+                        </button>
+                        <button className="bell">
+                            <BsBellFill />
+                        </button>
+                    </div>
+                    </>
+                )}
             </div>
         </header>
     );
-}
+};
 
 export default LandingHeader;
